ce511891d02ed2fc3fe9ad996d32f7c820e7867f
Cleberson Jesus <cleberson.jbabrros@outlook.com>
2019-08-11 15:45:59 -0300

    Adicionado um laço aprendendo git

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Ola pessoal");
+		
+		for (int i =0; i < 6 ;i++) {
+			System.out.println(i + " Aprendendo git");
+		}
 
 	}
 
