4bee3c39088f8aa4fc85d59c6237d6a59506be86
Cleberson Jesus <cleberson.jbabrros@outlook.com>
2019-08-11 15:56:15 -0300

    Adiciionado uma condicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,10 @@ public class Program {
 		System.out.println("Ola pessoal");
 		
 		for (int i =0; i < 6 ;i++) {
-			System.out.println(i + " Aprendendo git");
+			System.out.println(i + "   Aprendendo git");
+			if(i == 5) {
+				System.out.println("Falta muito para aprender o git ainda!");
+			}
 		}
 
 	}
